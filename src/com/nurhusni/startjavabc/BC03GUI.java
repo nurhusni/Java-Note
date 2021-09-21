c13caf0cc89da1a8dd4946ed0f15e8f30ab2d930
Panji Ahmad Nurhusni <apanji@hotmail.co.id>
2021-09-22 01:33:47 +0700

    Added changes

--- src/com/nurhusni/startjavabc/BC03GUI.java
+++ src/com/nurhusni/startjavabc/BC03GUI.java
@@ -14,7 +14,9 @@ public class BC03GUI {
         // showInputDialog returns string, you need to parse the return value
         int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
         double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height (in cm)"));
-        JOptionPane.showMessageDialog(null,
-                "You're " + age + " years old and " + height + " cm tall");
+        JOptionPane.showMessageDialog(
+                null,
+                "You're " + age + " years old and " + height + " cm tall"
+        );
     }
 }
