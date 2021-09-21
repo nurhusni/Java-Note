215c3690c69fd1e7c15a636b52419b702374ee92
Panji Ahmad Nurhusni <apanji@hotmail.co.id>
2021-09-22 02:00:55 +0700

    Added changes

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Java Note
 
-My note when learning Java. My main source is from [BroCode's Java Full Course on YouTube](https://www.youtube.com/watch?v=xk4_1vDrzzo) and other additional sources like [MOOC.fi](https://java-programming.mooc.fi/). I use Intellij IDEA for IDE. I use Java 17.
+- My note when learning Java.
+- Java version is Java 17.
+- The IDE is Intellij IDEA.
+
+# Sources
+- [BroCode's Java Full Course](https://www.youtube.com/watch?v=xk4_1vDrzzo) (main source).
+- [MOOC.fi](https://java-programming.mooc.fi).
+- [GeeksForGeeks](https://www.geeksforgeeks.org).
--- src/com/nurhusni/startjava/Learn000Basic.java
+++ src/com/nurhusni/startjava/Learn000Basic.java
@@ -10,17 +10,17 @@ public class Learn000Basic {
         // Data types and how to assign variables
         // String is reference type (stores data)
         // The others are primitive types (stores address in the memory)
-        boolean booleanType = true; // 1 bit true or false
+        boolean booleanType = true; // 1 bit | true or false
 
         byte byteType = 127; // 1 byte | -127 to 127
-        short shortType = 32767; // 2 bytes | -32768 to 32767
-        int integerType = 2000000000; // 4 bytes | 2 billion
-        long longType = 9000000000000000000L; // 8 bytes | 9 quintillion | 'L' at the end
+        short shortType = 32767; // 2 bytes | -32,768 to 32,767
+        int integerType = 2147483647; // 4 bytes | from -2,147,483,648
+        long longType = 9223372036854775807L; // 8 bytes | from -9,223,372,036,854,775,808 | ends with 'L'
 
-        float floatType = 3.141592f; // 4 bytes | 6-7 digits | 'f' at the end
-        double doubleType = 3.14653589793; // 8 bytes | 15 digits
+        float floatType = 3.141592f; // 4 bytes | 6-7 decimal digits | ends with 'f'
+        double doubleType = 3.14653589793; // 8 bytes | 15 decimal digits
 
-        char characterType = 'a'; // 2 bytes | single character | single quotes
+        char characterType = 'a'; // 2 bytes | single character or ASCII values | single quotes
         String stringType = "kata"; // various | multiple characters | double quotes
 
         // Escape sequences
