53a04a35850ad9d1bd9fb47743455a591eb4136c
Panji Ahmad Nurhusni <apanji@hotmail.co.id>
2021-09-22 01:47:55 +0700

    Added changes

--- src/com/nurhusni/startjava/Learn000Basic.java
+++ src/com/nurhusni/startjava/Learn000Basic.java
@@ -4,7 +4,7 @@ public class Learn000Basic {
 
     public static void main(String[] args) {
 
-        // Note from learning Java from BroCode
+        // Note from learning Java
         System.out.println("Hello World!");
 
         // Data types and how to assign variables
@@ -12,16 +12,16 @@ public class Learn000Basic {
         // The others are primitive types (stores address in the memory)
         boolean booleanType = true; // 1 bit true or false
 
-        byte byteType = 127; // 1 byte -127 to 127
-        short shortType = 32767; // 2 bytes -32768 to 32767
-        int integerType = 2000000000; // 4 bytes 2 billion
-        long longType = 9000000000000000000L; // 8 bytes 9 quintillion with 'L' at the end
+        byte byteType = 127; // 1 byte | -127 to 127
+        short shortType = 32767; // 2 bytes | -32768 to 32767
+        int integerType = 2000000000; // 4 bytes | 2 billion
+        long longType = 9000000000000000000L; // 8 bytes | 9 quintillion | 'L' at the end
 
-        float floatType = 3.141592f; // 4 bytes 6-7 digits with 'f' at the end
-        double doubleType = 3.14653589793; // 8 bytes 15 digits
+        float floatType = 3.141592f; // 4 bytes | 6-7 digits | 'f' at the end
+        double doubleType = 3.14653589793; // 8 bytes | 15 digits
 
-        char characterType = 'a'; // 2 bytes single character
-        String stringType = "kata";
+        char characterType = 'a'; // 2 bytes | single character | single quotes
+        String stringType = "kata"; // various | multiple characters | double quotes
 
         // Escape sequences
         String newLine = "\n";
