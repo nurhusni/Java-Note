9c3199314b0e36d2ca6e1bd346dcd601c5c5647e
Panji Ahmad Nurhusni <apanji@hotmail.co.id>
2021-09-22 01:32:24 +0700

    Added changes

--- src/com/nurhusni/startjavabc/BC01Basic.java
+++ src/com/nurhusni/startjavabc/BC01Basic.java
@@ -8,17 +8,25 @@ public class BC01Basic {
         System.out.println("Hello World!");
 
         // Data types and how to assign variables
-        byte bita = 120;
-        short pendek = 1256;
-        int bilbul = 1253681;
-        long panjang = 3186187361873618731L;
+        // String is reference type (stores data)
+        // The others are primitive types (stores address in the memory)
+        boolean booleanType = true; // 1 bit true or false
 
-        float ambang = 3.14f;
-        double ganda = 3.14;
+        byte byteType = 127; // 1 byte -127 to 127
+        short shortType = 32767; // 2 bytes -32768 to 32767
+        int integerType = 2000000000; // 4 bytes 2 billion
+        long longType = 9000000000000000000L; // 8 bytes 9 quintillion with 'L' at the end
 
-        boolean benarSalah = true;
+        float floatType = 3.141592f; // 4 bytes 6-7 digits with 'f' at the end
+        double doubleType = 3.14653589793; // 8 bytes 15 digits
 
-        char karakter = 'a';
-        String kata = "kata";
+        char characterType = 'a'; // 2 bytes single character
+        String stringType = "kata";
+
+        // Escape sequences
+        String newLine = "\n";
+        String tab = "\t";
+        String doubleQuote = "\"";
+        String backslash = "\\";
     }
 }
