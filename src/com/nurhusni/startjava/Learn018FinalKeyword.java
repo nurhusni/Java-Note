a422eb7cad82d24cfe079389ab531e634cb51294
Panji Ahmad Nurhusni <apanji@hotmail.co.id>
2021-10-17 01:03:07 +0700

    Added 018

---
+++ src/com/nurhusni/startjava/Learn018FinalKeyword.java
@@ -0,0 +1,16 @@
+package com.nurhusni.startjava;
+
+public class Learn018FinalKeyword {
+
+    public static void main(String[] args) {
+
+        // Final keyword makes variables constant
+        // The values can't be changed or updated
+        // Naming convention for final values is to make the name UPPERCASE
+
+        final double EULERS_NUMBER = 2.71828;
+
+        // EULERS_NUMBER = 2.72; // This gives error because it can't be updated
+
+    }
+}
